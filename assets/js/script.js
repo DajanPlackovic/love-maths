dd6aa389eb6518ae823ebce2d2b0bf0c2fd1d9ad
Dajan Plackovic <plackovic.dajan@gmail.com>
2024-02-24 11:18:37 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function runGame(gameType) {
 
   if (gameType === 'addition') {
     displayAdditionQuestion(num1, num2)
+  } else if (gameType === 'multiplication') {
+    displayMultiplyQuestion(num1, num2)
   } else {
     alert(`Unknown game type: ${gameType}`)
     throw `Unknown game type: ${gameType}. Aborting!`
@@ -64,6 +66,8 @@ function calculateCorrectAnswer() {
 
   if (operator === '+') {
     return [operand1 + operand2, 'addition']
+  } else if (operator === 'x') {
+    return [operand1 * operand2, 'multiplication']
   } else {
     alert(`Unimplemented operator: ${operator}`)
     throw `Unimplemented operator: ${operator}`
@@ -94,4 +98,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1
+  document.getElementById('operand2').textContent = operand2
+  document.getElementById('operator').textContent = 'x'
+}
